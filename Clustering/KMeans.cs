ca6046060d6c15d8b1a1e6c7bf28a4e79b3aa5c1
dropsonic <dropsonic.pan@gmail.com>
2013-10-27 14:20:24 +0400

    Basic class structure, unit tests

---
+++ Clustering/KMeans.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.Clustering
+{
+    public class KMeans<T>
+    {
+        Func<T, T, double> _distanceFunc;
+
+        public KMeans(Func<T, T, double> distanceFunc)
+        {
+            _distanceFunc = distanceFunc;
+        }
+
+        /// <param name="n">Количество кластеров.</param>
+        public IList<IList<T>> Clusterize(int n, IList<T> points)
+        {
+            if (n > points.Count)
+                throw new ArgumentException("Количество кластеров не может быть больше количества входных элементов.");
+
+            if (points == null)
+                throw new ArgumentNullException("data");
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClusteringTests/KMeansTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Thesis.Clustering;
+
+namespace ClusteringTests
+{
+    [TestClass]
+    public class KMeansTest
+    {
+        struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public Point(double x, double y)
+                : this()
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+        readonly Func<Point, Point, double> _distance = (a, b) =>
+                {
+                    return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+                };
+
+        Point[] GetData()
+        {
+            return new Point[] 
+            { 
+                new Point(1, 1),
+                new Point(1, 1.1),
+                new Point(3, 1),
+                new Point(2.9, 1),
+                new Point(1, 4),
+                new Point(4, 4)
+            };
+        }
+
+        [TestMethod]
+        public void ClusterizeTest()
+        {
+            var kmeans = new KMeans<Point>(_distance);
+            var clusters = kmeans.Clusterize(4, GetData());
+            Assert.AreEqual(4, clusters.Count);
+
+            Assert.IsTrue(clusters.Any(items => items.Any(p => p.X == 1 && p.Y == 1) 
+                && items.Any(p => p.X == 1 && p.Y == 1.1)));
+
+            Assert.IsTrue(clusters.Any(items => items.Any(p => p.X == 3 && p.Y == 1)
+                && items.Any(p => p.X == 2.9 && p.Y == 1)));
+
+            Assert.IsTrue(clusters.Any(items => items.Any(p => p.X == 1 && p.Y == 4)));
+
+            Assert.IsTrue(clusters.Any(items => items.Any(p => p.X == 4 && p.Y == 4)));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ClusterizeArgTest()
+        {
+            var kmeans = new KMeans<Point>(_distance);
+            var clusters = kmeans.Clusterize(7, GetData());
+        }
+    }
+}
