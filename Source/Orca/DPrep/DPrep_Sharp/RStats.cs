64f1b4a10ec238aae69210a80fb5b08751f9ab36
dropsonic <dropsonic.pan@gmail.com>
2013-11-08 22:56:01 +0400

    Docs: image and table captions

---
+++ Source/Orca/DPrep/DPrep_Sharp/RStats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.DPrep
+{
+    class RStats
+    {
+        public IList<float> Max { get; set; }
+        public IList<float> Min { get; set; }
+        public IList<float> Mean { get; set; }
+        public IList<float> Std { get; set; }
+
+        public RStats(int size)
+        {
+            //if (size < 1
+        }
+    }
+}
