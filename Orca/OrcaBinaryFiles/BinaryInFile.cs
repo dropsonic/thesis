3e62a331d02a121b6352ba6975f5aff4f3f67a0e
Vladimir Panchenko <dropsonic.pan@gmail.com>
2013-11-22 21:58:47 +0400

    Orca: outlier struct

---
+++ Orca/OrcaBinaryFiles/BinaryInFile.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.Orca.BinaryFiles
+{
+    /// <summary>
+    /// Represents Orca format binary file reader.
+    /// </summary>
+    public class BinaryInFile : IDisposable
+    {
+        BinaryReader _infile;
+
+        public int Index { get; set; }
+
+        public int Records { get; private set; }
+        public int RealFieldsCount { get; private set; }
+        public int DiscreteFieldsCount { get; private set; }
+
+        public float[] RealWeights { get; private set; }
+        public int[] DiscreteWeights { get; private set; }
+
+        private int HeaderSize
+        {
+            get
+            {
+                return 3 * sizeof(int) + RealFieldsCount * sizeof(float) + DiscreteFieldsCount * sizeof(int);
+            }
+        }
+
+        public BinaryInFile(string filename)
+        {
+            _infile = new BinaryReader(File.OpenRead(filename));
+            ReadHeader();
+        }
+
+        private void ReadHeader()
+        {
+            long oldPos = _infile.BaseStream.Position;
+
+            _infile.BaseStream.Position = 0;
+            Records = _infile.ReadInt32();
+            RealFieldsCount = _infile.ReadInt32();
+            DiscreteFieldsCount = _infile.ReadInt32();
+
+            RealWeights = _infile.ReadFloatArray(RealFieldsCount);
+            DiscreteWeights = _infile.ReadIntArray(DiscreteFieldsCount);
+
+            _infile.BaseStream.Position = oldPos;
+        }
+
+        public void SeekPosition(int pos)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(pos >= 0);
+            Contract.Requires<ArgumentOutOfRangeException>(pos < Records);
+
+            long filepos = HeaderSize + pos * RealFieldsCount * sizeof(float) + pos * DiscreteFieldsCount * sizeof(int);
+            _infile.BaseStream.Position = filepos;
+            Index = pos;
+        }
+
+        public void GetNextRecord(out int id, out float[] real, out int[] discrete)
+        {
+            // WARNING does not check to make sure read command succeeded.
+            id = _infile.ReadInt32();
+            real = _infile.ReadFloatArray(RealFieldsCount);
+            discrete = _infile.ReadIntArray(DiscreteFieldsCount);
+
+            Index++;
+        }
+
+        public void GetRecord(int pos, out int id)
+        {
+            SeekPosition(pos);
+            id = _infile.ReadInt32();
+            _infile.BaseStream.Position += RealFieldsCount * sizeof(float) + DiscreteFieldsCount * sizeof(int);
+            Index++;
+        }
+
+        public void GetRecord(int pos, out float[] real, out int[] discrete)
+        {
+            SeekPosition(pos);
+            int id;
+            GetNextRecord(out id, out real, out discrete); 
+        }
+
+        public void GetRecord(int pos, out int id, out float[] real, out int[] discrete)
+        {
+            SeekPosition(pos);
+            GetNextRecord(out id, out real, out discrete);
+        }
+
+        /// <summary>
+        /// Executes action for each record in file.
+        /// </summary>
+        /// <param name="action"></param>
+        public void ForEach(Action<int, float[], int[]> action)
+        {
+            SeekPosition(0);
+            while (Index < Records)
+            {
+                int id;
+                float[] real;
+                int[] discrete;
+                GetNextRecord(out id, out real, out discrete);
+                action(id, real, discrete);
+            }
+        }
+
+        #region IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+ 
+        private bool m_Disposed = false;
+ 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!m_Disposed)
+            {
+                if (disposing)
+                {
+                // Managed resources are released here.
+                    _infile.Close();
+                }
+ 
+                // Unmanaged resources are released here.
+                m_Disposed = true;
+            }
+        }
+ 
+        ~BinaryInFile()    
+        {        
+            Dispose(false);
+        }
+        #endregion
+    }
+}
---
+++ Orca/OrcaBinaryFiles/BinaryOutFile.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics.Contracts;
+
+namespace Thesis.Orca.BinaryFiles
+{
+    /// <summary>
+    /// Represents Orca format binary file writer.
+    /// </summary>
+    public class BinaryOutFile : IDisposable
+    {
+        BinaryWriter _outfile;
+
+        bool _headerWritten = false;
+
+        float[] _realWeights;
+        float[] _discreteWeights;
+
+        public int RealFieldsCount { get; private set; }
+        public int DiscreteFieldsCount { get; private set; }
+
+        public BinaryOutFile(string filename, float[] realWeights, float[] discreteWeights)
+        {
+            Contract.Requires(!String.IsNullOrEmpty(filename));
+            Contract.Requires<ArgumentNullException>(realWeights != null);
+            Contract.Requires<ArgumentNullException>(discreteWeights != null);
+
+            _outfile = new BinaryWriter(File.Create(filename));
+
+            _realWeights = realWeights;
+            _discreteWeights = discreteWeights;
+            RealFieldsCount = realWeights.Length;
+            DiscreteFieldsCount = discreteWeights.Length;
+
+            WriteHeader();
+        }
+
+        private void WriteHeader()
+        {
+            //long oldPos = _outfile.BaseStream.Position;
+
+            _outfile.Seek(0, SeekOrigin.Begin);
+            _outfile.Write((int)0); // number of records
+            _outfile.Write(RealFieldsCount);
+            _outfile.Write(DiscreteFieldsCount);
+            _outfile.Write(_realWeights);
+            _outfile.Write(_discreteWeights);
+
+            //_outfile.BaseStream.Position = oldPos;
+            _headerWritten = true;
+        }
+
+        public void WriteHeader(int numRecords)
+        {
+            if (!_headerWritten)
+                WriteHeader();
+
+            long oldPos = _outfile.BaseStream.Position;
+            _outfile.Seek(0, SeekOrigin.Begin);
+            _outfile.Write(numRecords);
+            _outfile.BaseStream.Position = oldPos;
+        }
+
+        public void WriteRecord(int id, float[] real, int[] discrete)
+        {
+            if (!_headerWritten)
+                WriteHeader();
+
+            _outfile.Write(id);
+            if (RealFieldsCount > 0)
+                _outfile.Write(real);
+            if (DiscreteFieldsCount > 0)
+                _outfile.Write(discrete);
+        }
+
+        public void Write(byte[] data)
+        {
+            if (!_headerWritten)
+                WriteHeader();
+
+            _outfile.Write(data);
+        }
+
+        #region IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+ 
+        private bool m_Disposed = false;
+ 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!m_Disposed)
+            {
+                if (disposing)
+                {
+                // Managed resources are released here.
+                    _outfile.Close();
+                }
+ 
+                // Unmanaged resources are released here.
+                m_Disposed = true;
+            }
+        }
+ 
+        ~BinaryOutFile()    
+        {        
+            Dispose(false);
+        }
+        #endregion
+    }
+}
