a8b2bb4672d89b5db6884664b56c445e53ba4e57
Vladimir Panchenko <dropsonic.pan@gmail.com>
2013-11-23 17:14:29 +0400

    Orca distance function

--- Orca/Orca_Sharp/Orca.cs
+++ Orca/Orca_Sharp/Orca.cs
@@ -102,11 +102,51 @@ namespace Thesis.Orca
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Calculates the distance between two examples with weights.
+        /// </summary>
         private double Distance(Record a, Record b, Weights weights)
         {
+            Contract.Requires(a.Real.Length == b.Real.Length);
+            Contract.Requires(a.Discrete.Length == b.Discrete.Length);
+            Contract.Requires(a.Real.Length == weights.Real.Length);
+            Contract.Requires(a.Discrete.Length == weights.Discrete.Length);
 
+            int realFieldsCount = a.Real.Length;
+            int discreteFieldsCount = a.Discrete.Length;
 
-            throw new NotImplementedException();
+            double d = 0;
+
+            // real 
+            for (int i = 0; i < realFieldsCount; i++)
+            {
+                // check for missing values
+                int missingCount = 0;
+                if (a.Real[i] == Parameters.MissingR)
+                    missingCount++;
+                if (b.Real[i] == Parameters.MissingR)
+                    missingCount++;
+
+                if (missingCount == 0)
+                {
+                    double diff = a.Real[i] - b.Real[i];
+                    d += diff * diff * weights.Real[i];
+                }
+                // one value is missing
+                else if (missingCount == 1)
+                {
+                    d += Parameters.DistMR;
+                }
+            }
+
+            // discrete
+            for (int i = 0; i < discreteFieldsCount; i++)
+            {
+                if (a.Discrete[i] != b.Discrete[i])
+                    d += weights.Discrete[i];
+            }
+
+            return d;
         }
     }
 }
