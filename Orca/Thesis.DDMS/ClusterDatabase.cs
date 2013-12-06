1677f6c6b1a6b65c1ee4556ad57f11c86e068c6e
Vladimir Panchenko <dropsonic.pan@gmail.com>
2013-12-06 22:03:11 +0400

    Cluster database class

---
+++ Orca/Thesis.DDMS/ClusterDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.DDMS
+{
+    class ClusterDatabase
+    {
+        private IList<Cluster> _cluster = new List<Cluster>();
+        private double _eps;
+
+        public ClusterDatabase(double eps)
+        {
+            _eps = eps;
+        }
+
+        public void AddRecord(Record record)
+        {
+
+        }
+    }
+}
