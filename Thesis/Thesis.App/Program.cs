324dd8a394fced8876fa2672f802b7995fa586a2
Vladimir Panchenko <dropsonic.pan@gmail.com>
2013-12-08 10:55:54 +0400

    Correct temp file deletion; input files existance checking

--- Thesis/Thesis.App/Program.cs
+++ Thesis/Thesis.App/Program.cs
@@ -34,10 +34,15 @@ namespace Thesis.App
             string fieldsFile = args[0];
             string dataFile = args[1];
             string shuffleFile = String.Concat(dataFile, ".shuffle");
-            IRecordParser<string> parser = new PlainTextParser();
 
             try
             {
+                foreach (string filename in args)
+                    if (!File.Exists(filename))
+                        throw new ApplicationException("File not found.");
+
+                IRecordParser<string> parser = new PlainTextParser();
+
                 using (IDataReader reader = new PlainTextReader(dataFile, fieldsFile, parser))      // read input data   
                 using (IDataReader binReader = new BinaryDataReader(reader, GetBinName(dataFile)))  // convert input data to binary format
                 {
@@ -68,68 +73,71 @@ namespace Thesis.App
                     using (ScaleDataReader scaleReader = new ScaleDataReader(binReader, scaling)) // scale input data
                     {
                         scaleReader.Shuffle(shuffleFile);
+                        IEnumerable<Outlier> outliers = Enumerable.Empty<Outlier>();
 
                         using (IDataReader cases = new BinaryDataReader(shuffleFile))
                         using (IDataReader references = new BinaryDataReader(shuffleFile))
                         {
                             var orca = new OrcaAD(DistanceFunctions.Euclid, neighborsCount: 10);
-                            var outliers = orca.Run(cases, references, true);
+                            outliers = orca.Run(cases, references, true);
+                        }
 
-                            IAnomaliesFilter filter = new GaussianFilter();
-                            //IAnomaliesFilter filter = new DifferenceFilter(0.05);
-                            var anomalies = filter.Filter(outliers);
+                        File.Delete(shuffleFile);
 
-                            Console.WriteLine("Anomalies:");
-                            foreach (var anomaly in anomalies)
-                                Console.WriteLine("  Id = {0}, Score = {1}", anomaly.Id, anomaly.Score);
+                        IAnomaliesFilter filter = new GaussianFilter();
+                        //IAnomaliesFilter filter = new DifferenceFilter(0.05);
+                        var anomalies = filter.Filter(outliers);
 
-                            Console.WriteLine();
+                        Console.WriteLine("Anomalies:");
+                        foreach (var anomaly in anomalies)
+                            Console.WriteLine("  Id = {0}, Score = {1}", anomaly.Id, anomaly.Score);
 
-                            using (IDataReader cleanReader = new CleanDataReader(scaleReader, anomalies)) // clean input data from anomalies
+                        Console.WriteLine();
+
+                        using (IDataReader cleanReader = new CleanDataReader(scaleReader, anomalies)) // clean input data from anomalies
+                        {
+                            model.AddRegime("Nominal", cleanReader);
+
+                            Console.WriteLine("Knowledge base has been created. {0} regime(s) total:", model.Regimes.Count);
+                            foreach (var regime in model.Regimes)
                             {
-                                model.AddRegime("Nominal", cleanReader);
-
-                                Console.WriteLine("Knowledge base has been created. {0} regime(s) total:", model.Regimes.Count);
-                                foreach (var regime in model.Regimes)
+                                Console.WriteLine("\n***** {0} *****", regime.Name);
+                                Console.WriteLine("{0} cluster(s) in regime.", regime.Clusters.Count);
+                                int i = 0;
+                                foreach (var cluster in regime.Clusters)
                                 {
-                                    Console.WriteLine("\n***** {0} *****", regime.Name);
-                                    Console.WriteLine("{0} cluster(s) in regime.", regime.Clusters.Count);
-                                    int i = 0;
-                                    foreach (var cluster in regime.Clusters)
-                                    {
-                                        Console.WriteLine("  --------------------------");
-                                        Console.WriteLine("  Cluster #{0}:", ++i);
-                                        Console.WriteLine("  Lower bound: {0}", String.Join(" | ", scaling.Unscale(cluster.LowerBound)));
-                                        Console.WriteLine("  Upper bound: {0}", String.Join(" | ", scaling.Unscale(cluster.UpperBound)));
-                                        Console.WriteLine("  Appropriate discrete values: {0}", String.Join(" | ", cluster.DiscreteValues.Select(f => String.Join("; ", f))));
-                                    }
                                     Console.WriteLine("  --------------------------");
-                                    Console.WriteLine("******************", regime.Name);
+                                    Console.WriteLine("  Cluster #{0}:", ++i);
+                                    Console.WriteLine("  Lower bound: {0}", String.Join(" | ", scaling.Unscale(cluster.LowerBound)));
+                                    Console.WriteLine("  Upper bound: {0}", String.Join(" | ", scaling.Unscale(cluster.UpperBound)));
+                                    Console.WriteLine("  Appropriate discrete values: {0}", String.Join(" | ", cluster.DiscreteValues.Select(f => String.Join("; ", f))));
+                                }
+                                Console.WriteLine("  --------------------------");
+                                Console.WriteLine("******************", regime.Name);
+                            }
+
+                            Console.WriteLine("\nEnter record, or press enter to quit.");
+
+                            string line = String.Empty;
+                            do
+                            {
+                                line = Console.ReadLine();
+                                if (String.IsNullOrEmpty(line)) break;
+
+                                var record = parser.Parse(line, cleanReader.Fields);
+                                if (record == null)
+                                {
+                                    Console.WriteLine("Wrong record format. Please enter record again.");
+                                    continue;
                                 }
 
-                                Console.WriteLine("\nEnter record, or press enter to quit.");
-
-                                string line = String.Empty;
-                                do
-                                {
-                                    line = Console.ReadLine();
-                                    if (String.IsNullOrEmpty(line)) break;
-
-                                    var record = parser.Parse(line, cleanReader.Fields);
-                                    if (record == null)
-                                    {
-                                        Console.WriteLine("Wrong record format. Please enter record again.");
-                                        continue;
-                                    }
-
-                                    scaling.Scale(record);
-                                    Regime currentRegime = model.DetectRegime(record);
-                                    if (currentRegime == null)
-                                        Console.WriteLine("Anomaly behavior detected.\n");
-                                    else
-                                        Console.WriteLine("Current regime: {0}\n", currentRegime.Name);
-                                } while (true);
-                            }
+                                scaling.Scale(record);
+                                Regime currentRegime = model.DetectRegime(record);
+                                if (currentRegime == null)
+                                    Console.WriteLine("Anomaly behavior detected.\n");
+                                else
+                                    Console.WriteLine("Current regime: {0}\n", currentRegime.Name);
+                            } while (true);
                         }
                     }
                 }
@@ -141,10 +149,6 @@ namespace Thesis.App
             catch (Exception ex)
             {
                 Console.WriteLine("Error: {0} Please contact the developer.", ex.Message);
-            }
-            finally
-            {
-                File.Delete(shuffleFile);
             }
         }
     }
